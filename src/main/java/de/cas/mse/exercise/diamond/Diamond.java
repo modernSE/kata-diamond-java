0e8998af3513a63f4ead5739a04b02cb2f33c468
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2018-02-20 09:47:55 +0100

    Ergebnis der Gruppe 4

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -1,31 +1,53 @@
 package de.cas.mse.exercise.diamond;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
-			return null;
+	
+	public String printDiamond(int numberOfLines) {
+		assertTrue ("Number of Lines in the Diamond must be positive", numberOfLines <= 0);
+		assertFalse("Number of Lines in the Diamond must be odd", numberOfLines % 2 == 0);
+		
+		StringBuilder diamond = new StringBuilder();
+		int middleLineNumber = numberOfLines / 2 + 1;
+		int lineLength = 0;
+		int numberOfWhitespaces = numberOfLines;
+		for (int i=0; i<numberOfLines; i++) {
+			if (i <middleLineNumber) {
+				numberOfWhitespaces = numberOfLines / 2 - i;
+				lineLength = middleLineNumber + i;
+				
+			} else {
+				numberOfWhitespaces = i/2 ;
+				lineLength = i;
+			}
+
+			diamond.append(printDiamondLine(lineLength, numberOfWhitespaces));
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
+		
+		return diamond.toString();
+		
+	}
+	
+	private String printDiamondLine(int lineLength, int numberOfWhitespaces) {
+		StringBuilder diamondline = new StringBuilder();
+		int numberOfStars = lineLength - numberOfWhitespaces ;
+		System.out.println("numberOfStars " + numberOfStars);
+		for (int i=0; i< numberOfWhitespaces; i++) {
+			diamondline.append(' ');
 		}
-		return builder.toString();
+		
+		for (int i=0; i< numberOfStars; i++) {
+			diamondline.append('*');
+		}
+
+		diamondline.append('\n');
+		return diamondline.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
-		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
-	}
 
 }
