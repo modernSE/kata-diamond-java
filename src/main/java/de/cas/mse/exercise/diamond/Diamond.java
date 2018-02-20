757eb8027ee68fde099aae36024d8a21117f3e5b
modernSE Trainee 1 <keydon+mse-trainee@gmail.com>
2018-02-20 10:00:15 +0100

    Ergebnis der Gruppe 1

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -3,28 +3,33 @@ package de.cas.mse.exercise.diamond;
 import java.util.Arrays;
 
 public class Diamond {
+	
+	// Idee: Statt StringBuilder LinkedList<String>, statt char[] komplett auf Strings umsteigen.
 
 	public String createDiamond(int height) {
-		if (height <= 0 || height % 2 == 0) {
+		if (height <= 0 || height % 2 == 0) {	// Funktion rausziehen, und sprechend bezeichnen: tooSmall() or notEven()
 			return null;
 		}
-		final StringBuilder result = new StringBuilder(new String(createLine(height, height)));
-		for (int i = height - 2; i > 0; i -= 2) {
-			char[] line = createLine(height, i);
-			result.insert(0, line);
+		// In der Zeile passieren zwei Sachen: Stringbuilder erstellen und mittlere Zeile einfügen. 
+		// Trennen, und für letzteres
+		// könnte man eine eigene Funktion machen.
+		final StringBuilder result = new StringBuilder(new String(createLine(height, height))); 
+		for (int i = height - 2; i > 0; i -= 2) {   	// i ist kein aussagender Bezeichner: numberOfStars?
+			char[] line = createLine(height, i);	// line -> symmetricLine
+			result.insert(0, line);			// Beide Zeilen in Funktion rausziehen: growDiamondVerticallyUpAndDown
 			result.append(line);
 		}
 		return result.toString();
 	}
 
 	private char[] createLine(int maxNumberOfStars, int numberOfStarsInLine) {
-		int numberOfBlanks = ((maxNumberOfStars - numberOfStarsInLine) / 2);
-		int numberOfVisibleCharacters = numberOfBlanks + numberOfStarsInLine;
+		int numberOfBlanks = ((maxNumberOfStars - numberOfStarsInLine) / 2);  // 
+		int numberOfVisibleCharacters = numberOfBlanks + numberOfStarsInLine; // Sehr GUt: +1 entfernt.
 		char[] line = new char[numberOfVisibleCharacters + 1];
-		if (numberOfBlanks > 0) {
-			Arrays.fill(line, 0, numberOfBlanks, ' ');
+		if (numberOfBlanks > 0) { // if komplett entfernen
+			Arrays.fill(line, 0, numberOfBlanks, ' ');	// String externalisieren, mit abstraktem Namen: OUTSIDE_SYMBOL
 		}
-		Arrays.fill(line, numberOfBlanks, numberOfVisibleCharacters, '*');
+		Arrays.fill(line, numberOfBlanks, numberOfVisibleCharacters, '*'); // String externalisieren: INSIDE_SYMBOL
 		line[numberOfVisibleCharacters] = '\n';
 		return line;
 	}
