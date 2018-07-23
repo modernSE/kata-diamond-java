8671f8c231046d8a35aed5d16370b5baf856a8e9
modernSE Trainee 2 <keydon+mse-trainee@gmail.com>
2018-07-23 16:30:37 +0200

    Ergebnis der Gruppe 2

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -1,31 +1,64 @@
 package de.cas.mse.exercise.diamond;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Applied object calisthenics:
+ * 1 OK
+ * 2 OK
+ * 3 
+ * 4 OK
+ * 5 OK
+ * 6 OK
+ * 7 OK
+ * 8 OK :)
+ * 9
+ * 
+ * @author modernSE
+ *
+ */
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	public String print(int size) {
+		Map<String, Integer> isPrintableParanmeters = new HashMap<>();
+		isPrintableParanmeters.put("size", size);
+		if (isPrintable(isPrintableParanmeters)) {
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
+		Map<String, Integer> makeLineParameters = new HashMap<>();
+		makeLineParameters.put("maximumAmountOfAsterisks", size);
+		makeLineParameters.put("amountOfAsterisks", size);
+		String middleLine = new String(makeLine(makeLineParameters));
+		StringBuilder builder = new StringBuilder(middleLine);
+		for (int amountOfAsterisks = size - 2; amountOfAsterisks > 0; amountOfAsterisks -= 2) {
+			makeLineParameters.put("amountOfAsterisks", amountOfAsterisks);
+			char[] chars = makeLine(makeLineParameters);
 			builder.insert(0, chars);
 			builder.append(chars);
 		}
 		return builder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
+	private boolean isPrintable(Map<String, Integer> parameters) {
+		int size = parameters.get("size");
+		return size <= 0 || size % 2 == 0;
+	}
+
+	private char[] makeLine(Map<String, Integer> parameters) {
+		int maximumAmountOfAsterisks = parameters.get("maximumAmountOfAsterisks");
+		int amountOfAsterisks = parameters.get("amountOfAsterisks");
+		int amount = ((maximumAmountOfAsterisks - amountOfAsterisks) / 2);
+		char[] characters = new char[amount + amountOfAsterisks + 1];
 		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+			
+			Arrays.fill(characters, 0, amount, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		Arrays.fill(characters, amount, amount + amountOfAsterisks, '*');
+		characters[characters.length - 1] = '\n';
+		return characters;
 	}
 
 }
