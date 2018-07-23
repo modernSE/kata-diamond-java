aaa3ed854057832bde559b651bb80442dc6ccfac
modernSE Trainee 5 <keydon+mse-trainee@gmail.com>
2018-07-23 09:52:52 +0200

    Ergebnis der Gruppe 5

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,28 +4,33 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	public String print(int maxWidth) { // Maybe "size" would be better because width is also the height
+		if (isNegativeOrEven(maxWidth)) { // isEven(maxWidth) || isNegative(maxWidth) (one method should only do one thing)
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
+		String middleRow = new String(createRow(maxWidth, maxWidth)); // sehr gut
+		StringBuilder builder = new StringBuilder(middleRow);
+		for (int amountOfStars = maxWidth - 2; amountOfStars > 0; amountOfStars -= 2) {
+			char[] row = createRow(maxWidth, amountOfStars);
+			builder.insert(0, row);
+			builder.append(row);
 		}
 		return builder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	private boolean isNegativeOrEven(int maxWidth) {
+		return maxWidth <= 0 || maxWidth % 2 == 0;
+	}
+
+	private char[] createRow(int maxWidth, int amountOfStars) {
+		int amountOfSpaces = ((maxWidth - amountOfStars) / 2);
+		char[] row = new char[amountOfSpaces + amountOfStars + 1];
+		if (amountOfSpaces > 0) {
+			Arrays.fill(row, 0, amountOfSpaces, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		Arrays.fill(row, amountOfSpaces, amountOfSpaces + amountOfStars, '*');
+		row[row.length - 1] = '\n';
+		return row;
 	}
 
 }
