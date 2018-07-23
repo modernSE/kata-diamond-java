9d50f2a4c5242238c47d6e52d5ac18b1228457bb
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2018-07-23 09:52:51 +0200

    Ergebnis der Gruppe 4

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,28 +4,38 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	public String print(int sizeOfDiamond) {
+		// Könnte man auslagen
+		if (sizeOfDiamond <= 0 || sizeOfDiamond % 2 == 0) {
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
+		StringBuilder builder = new StringBuilder(new String(makeRow(sizeOfDiamond, sizeOfDiamond)));
+		for (int rowIndex = sizeOfDiamond - 2; rowIndex > 0; rowIndex -= 2) {
+			// rowIndex ist nicht klar, da Rows oben und unten angehängt werden
+			// Vorschlag: hier schon numOfStars nennen
+
+			char[] row = makeRow(sizeOfDiamond, rowIndex);
+			builder.insert(0, row);
+			builder.append(row);
 		}
 		return builder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	private char[] makeRow(int totalNumOfStars, int numOfStars) {
+		// totalNumOfStars ist nicht eindeutig, man könnte hier auch eine Summe vermuten
+		// Vorschlag: maxNumOfStars
+		// wieso nicht sizeOfDiamond wie in Line 12? 
+		// 
+		int numOfSpaces = ((totalNumOfStars - numOfStars) / 2);
+		char[] row = new char[numOfSpaces + numOfStars + 1];
+		if (numOfSpaces > 0) {
+			Arrays.fill(row, 0, numOfSpaces, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		Arrays.fill(row, numOfSpaces, numOfSpaces + numOfStars, '*');
+		row[row.length - 1] = '\n';
+		// Man könnte hier schon Row als String zurückgeben,
+		// damit man sich das new String in Line 12 spart		
+		return row;
 	}
 
 }
