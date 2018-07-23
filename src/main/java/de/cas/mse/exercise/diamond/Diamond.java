52e7f03c4e27b3f0e917599c0d2b2d423943a1cb
modernSE Trainee 3 <keydon+mse-trainee@gmail.com>
2018-07-23 16:30:36 +0200

    Ergebnis der Gruppe 3

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -1,31 +1,50 @@
 package de.cas.mse.exercise.diamond;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	
+	// - Keine Abkürzungen
+	// - Alle primitiven Datentypen und Strings wrappen
+	// - Collections als ertklassiges Konzept
+	public String print(Integer size) {
+		if (size <= 0 || size % 2 == 0) {
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
+		List<Character> middleLine = make(size, size);
+		Deque<List<Character>> lines = new ArrayDeque<>();
+		lines.add(middleLine);
+		for (Integer index = size - 2; index > 0; index -= 2) {
+			List<Character> line = make(size, index);
+			lines.addFirst(line);
+			lines.addLast(line);
 		}
-		return builder.toString();
+
+		StringBuilder sb = new StringBuilder();
+		lines.stream().flatMap(line -> line.stream()).forEach(sb::append);
+		return sb.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
+	private List<Character> make(int size, int numberOfStars) {
+		int amount = ((size - numberOfStars) / 2);
+		List<Character> characters = new ArrayList<>(amount + numberOfStars + 1);
 		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+			for (int i = 0; i < amount; i++) {
+				characters.add(' ');
+			}
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		for(int i = 0; i < numberOfStars; i++) {
+			characters.add('*');
+		}
+		characters.add('\n');
+		return characters;
 	}
-
 }
