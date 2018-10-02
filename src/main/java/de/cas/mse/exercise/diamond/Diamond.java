af775d152201dbe6751c2246dbc5cb9e0c03b0da
modernSE Trainee 3 <keydon+mse-trainee@gmail.com>
2018-10-02 10:28:53 +0200

    Ergebnis der Gruppe 3

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,28 +4,46 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	// V+M: diamond prefix is unnecessary +1
+	// V+M: can also be size or dimension
+	// V+M: is not a print method. Can be called createDiamond or buildDiamond +1
+	public String print(int diamonWidth) {
+		// extract method: isPrintable?
+		if (diamonWidth <= 0 || diamonWidth % 2 == 0) {
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
+		StringBuilder diamondBuilder = new StringBuilder(new String(createDiamondLayer(diamonWidth, diamonWidth)));
+		for (int i = diamonWidth - 2; i > 0; i -= 2) {
+			char[] diamondLayer = createDiamondLayer(diamonWidth, i);
+			diamondBuilder.insert(0, diamondLayer);
+			diamondBuilder.append(diamondLayer);
 		}
-		return builder.toString();
+		
+		// V+M: Remove test code +1
+		System.out.println(diamondBuilder.toString());
+		
+		return diamondBuilder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	private char[] createDiamondLayer(int diamondWidth, int amountOfStars) {
+		// V+M: maybe numberOfSpaces
+		// amountOfSpaces as amount was used in parameter as well
+		int startIndex = ((diamondWidth - amountOfStars) / 2);
+		
+		// V+M: for consistency layer would be a better name
+		char[] chars = new char[startIndex + amountOfStars + 1];
+		if (startIndex > 0) {
+			Arrays.fill(chars, 0, startIndex, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
+		Arrays.fill(chars, startIndex, startIndex + amountOfStars, '*');
 		chars[chars.length - 1] = '\n';
 		return chars;
+	}
+	
+	// Remove test code 
+	public static void main(String[] args) {
+		Diamond diamond = new Diamond();
+		diamond.print(5);
 	}
 
 }
