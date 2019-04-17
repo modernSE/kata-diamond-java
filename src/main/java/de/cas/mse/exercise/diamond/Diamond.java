47a21441fb131d74397c34845a33def4c11aac6d
modernSE Trainee 5 <keydon+mse-trainee@gmail.com>
2019-04-17 14:30:32 +0200

    Ergebnis der Gruppe 5

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,28 +4,39 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
-			return null;
+	public static void main(String[] args) {
+		Diamond test = new Diamond();
+		System.out.println(test.getDiamond(555));
+	}
+	
+	// Review: Diamand im Methodennamen überflüssig
+	// builder -> result z.B.
+	// chars -> line oder currentLine
+	
+	public String getDiamond(int maxWidth) {
+		if (maxWidth <= 0 || maxWidth % 2 == 0) {
+			return "Wrong Number"; 
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
+		StringBuilder builder = new StringBuilder(new String(makeLine(maxWidth, maxWidth)));
+		for (int currentWidth = maxWidth - 2; currentWidth > 0; currentWidth -= 2) {
+			char[] chars = makeLine(maxWidth, currentWidth);
 			builder.insert(0, chars);
 			builder.append(chars);
 		}
 		return builder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	// maxWidth -> width oder lineWidth 	currentWidth -> currentDiamondWidth oder amountStars
+	// offsetFromMiddleLine -> offsetFromLeft oder einfach offset
+	private char[] makeLine(int maxWidth, int currentWidth) {
+		int offsetFromMiddleLine = ((maxWidth - currentWidth) / 2);
+		char[] line = new char[offsetFromMiddleLine + currentWidth + 1];
+		if (offsetFromMiddleLine > 0) {
+			Arrays.fill(line, 0, offsetFromMiddleLine, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		Arrays.fill(line, offsetFromMiddleLine, offsetFromMiddleLine + currentWidth, '*');
+		line[line.length - 1] = '\n';
+		return line;
 	}
 
 }
