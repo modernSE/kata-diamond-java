cf47323785321b7ba3ff9702d0bff71340079bb9
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2020-03-13 15:03:53 +0100

    Review der Gruppe 4

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int maxWidth) {
-		if (maxWidth <= 0 || maxWidth % 2 == 0) {
+	public String print(int maxWidth) {	//diamondSize would be more pregnant, since width and height are the same for diamonds
+		if (maxWidth <= 0 || maxWidth % 2 == 0) { //extract method for validation check
 			return null;
 		}
-		StringBuilder star = createStar(maxWidth);
+		StringBuilder star = createStar(maxWidth);//is this a single * ? We print a diamond!
 		return star.toString();
 	}
 
 	private StringBuilder createStar(int maxWidth) {
 		StringBuilder star = new StringBuilder();
-		addCenterLine(star, maxWidth);
+		addCenterLine(star, maxWidth); //sideeffect changes star
 		addSurroundingLines(star, maxWidth);
 		return star;
 	}
 
 	private void addSurroundingLines(StringBuilder star, int maxWidth) {
-		int currentNumberOfStars = maxWidth - 2;
-		while (currentNumberOfStars > 0) {
+		int currentNumberOfStars = maxWidth - 2; 
+		while (currentNumberOfStars > 0) { //nice change <3
 			addLine(star, maxWidth, currentNumberOfStars);
 			currentNumberOfStars -= 2;
 		}
@@ -37,13 +37,13 @@ public class Diamond {
 		star.append(line);
 	}
 
-	private String createLine(int totalWidth, int numberOfStars) {
+	private String createLine(int totalWidth, int numberOfStars) { //this is the same as maxWidth
 		int numberOfSpaces = ((totalWidth - numberOfStars) / 2);
-		char[] chars = new char[numberOfSpaces + numberOfStars + 1];
-		if (numberOfSpaces > 0) {
+		char[] chars = new char[numberOfSpaces + numberOfStars + 1]; //chars is a row
+		if (numberOfSpaces > 0) { //own method to add whitespaces
 			Arrays.fill(chars, 0, numberOfSpaces, ' ');
 		}
-		Arrays.fill(chars, numberOfSpaces, numberOfSpaces + numberOfStars, '*');
+		Arrays.fill(chars, numberOfSpaces, numberOfSpaces + numberOfStars, '*'); //own method to add stars
 		chars[chars.length - 1] = '\n';
 		return new String(chars);
 	}
