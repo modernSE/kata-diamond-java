065baffed79a929d89e53c6df3fe44981e382e07
modernSE Trainee 5 <keydon+mse-trainee@gmail.com>
2020-03-13 15:09:03 +0100

    Review 2 der Gruppe 5

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -14,6 +14,7 @@ public class Diamond {
 		// Missing abstraction e.g  buildCenterLine(..) to avoid confusion on double param usage
 		StringBuilder diamond = new StringBuilder(new String(buildLine(diamondSize, diamondSize)));
 		
+		//prefix 'amount' not really necessary
 		for (int amountStarsOfLine = diamondSize - 2; amountStarsOfLine > 0; amountStarsOfLine -= 2) {
 			// Better to use strings for simplicity outside of buildLine(..)
 			// Could be abstracted into a well named method e.g. expandDiamondUpAndDown
@@ -31,6 +32,7 @@ public class Diamond {
 
 	
 	private char[] buildLine(int totalLength, int amountStars) {
+		//prefix 'amount' not really necessary
 		int amountLeadingWhitespaces = ((totalLength - amountStars) / 2);
 		char[] line = new char[amountLeadingWhitespaces + amountStars + 1];
 		// Extract next 3 lines e.g.  addLeadingWhitespace(..) 
@@ -39,7 +41,7 @@ public class Diamond {
 		}
 		// Extract next line for readability to method
 		Arrays.fill(line, amountLeadingWhitespaces, amountLeadingWhitespaces + amountStars, '*');
-		// Extract as well for consistencz
+		// Extract as well for consistency
 		line[line.length - 1] = '\n';
 		return line;
 	}
