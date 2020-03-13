5c9fa6285870526a6bed6b96282c6c4e387815d9
modernSE Trainee 2 <keydon+mse-trainee@gmail.com>
2020-03-13 14:48:51 +0100

    Ergebnis der Gruppe 2

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -4,28 +4,74 @@ import java.util.Arrays;
 
 public class Diamond {
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	public String print(int diamondHeight) {
+		if (isValidHeight(diamondHeight)) {
 			return null;
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
+		String middleRow = buildMiddleRow(diamondHeight);
+		StringBuilder builder = new StringBuilder(middleRow);
+		
+		for (int currentWidth = diamondHeight - 2; currentWidth > 0; currentWidth -= 2) {
+			builder = addRowsSymetrically(diamondHeight, builder, currentWidth);
 		}
 		return builder.toString();
 	}
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	private StringBuilder addRowsSymetrically(int totalWidth, StringBuilder builder, int currentWidth) {
+		String currentRow = buildRow(totalWidth, currentWidth);
+		addRowAbove(builder, currentRow);
+		addRowBelow(builder, currentRow);
+		return builder;
+	}
+
+	private String buildMiddleRow(int diamondHeight) {
+		return buildRow(diamondHeight, diamondHeight);
+	}
+
+	private StringBuilder addRowBelow(StringBuilder builder, String currentRow) {
+		return builder.append(currentRow);
+	}
+
+	private void addRowAbove(StringBuilder builder, String currentRow) {
+		builder.insert(0, currentRow);
+	}
+
+	private boolean isValidHeight(int diamondHeight) {
+		return diamondHeight <= 0 || diamondHeight % 2 == 0;
+	}
+
+	private String buildRow(int totalDiamondWidth, int numberOfStars) {
+		
+		int whiteSpacesOnOneSide = (totalDiamondWidth-numberOfStars) / 2;
+		char[] row = new char[calculateRowWidth(numberOfStars, whiteSpacesOnOneSide)];
+		
+		row = fillWhiteSpaces(whiteSpacesOnOneSide, row);
+		
+		row = fillStars(numberOfStars, whiteSpacesOnOneSide, row);
+		row = appendLineBreak(row);
+		
+		return new String(row);
+	}
+
+	private char[] appendLineBreak(char[] row) {
+		row[row.length - 1] = '\n';
+		return row;
+	}
+
+	private char[] fillStars(int numberOfStars, int whiteSpacesOnOneSide, char[] row) {
+		Arrays.fill(row, whiteSpacesOnOneSide, whiteSpacesOnOneSide + numberOfStars, '*');
+		return row;
+	}
+
+	private char[] fillWhiteSpaces(int whiteSpacesOnOneSide, char[] row) {
+		if (whiteSpacesOnOneSide > 0) {
+			Arrays.fill(row, 0, whiteSpacesOnOneSide, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		return row;
+	}
+
+	private int calculateRowWidth(int numberOfStars, int whiteSpacesOnOneSide) {
+		return whiteSpacesOnOneSide + numberOfStars + 1;
 	}
 
 }
