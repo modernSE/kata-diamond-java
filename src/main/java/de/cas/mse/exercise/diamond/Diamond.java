9c62a4ebf1b302e49a0e8b43ae4f135d601a3546
modernSE Trainee 5 <keydon+mse-trainee@gmail.com>
2020-03-13 15:03:53 +0100

    Review der Gruppe 5

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -5,11 +5,18 @@ import java.util.Arrays;
 public class Diamond {
 
 	public String createDiamond(int diamondSize) {
+		// GOOD
 		if (isInvalid(diamondSize)) {
 			return null;
 		}
+		
+		// Line is too long, use interm. variable f. clarity
+		// Missing abstraction e.g  buildCenterLine(..) to avoid confusion on double param usage
 		StringBuilder diamond = new StringBuilder(new String(buildLine(diamondSize, diamondSize)));
+		
 		for (int amountStarsOfLine = diamondSize - 2; amountStarsOfLine > 0; amountStarsOfLine -= 2) {
+			// Better to use strings for simplicity outside of buildLine(..)
+			// Could be abstracted into a well named method e.g. expandDiamondUpAndDown
 			char[] line = buildLine(diamondSize, amountStarsOfLine);
 			diamond.insert(0, line);
 			diamond.append(line);
@@ -17,17 +24,22 @@ public class Diamond {
 		return diamond.toString();
 	}
 
+	// GOOD
 	private boolean isInvalid(int diamondSize) {
 		return diamondSize <= 0 || diamondSize % 2 == 0;
 	}
 
+	
 	private char[] buildLine(int totalLength, int amountStars) {
 		int amountLeadingWhitespaces = ((totalLength - amountStars) / 2);
 		char[] line = new char[amountLeadingWhitespaces + amountStars + 1];
+		// Extract next 3 lines e.g.  addLeadingWhitespace(..) 
 		if (amountLeadingWhitespaces > 0) {
 			Arrays.fill(line, 0, amountLeadingWhitespaces, ' ');
 		}
+		// Extract next line for readability to method
 		Arrays.fill(line, amountLeadingWhitespaces, amountLeadingWhitespaces + amountStars, '*');
+		// Extract as well for consistencz
 		line[line.length - 1] = '\n';
 		return line;
 	}
