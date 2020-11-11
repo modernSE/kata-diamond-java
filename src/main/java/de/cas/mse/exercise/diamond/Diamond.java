039f7c3c10b68deb3645632936c3a7a5f7c02cda
TehMillhouse <max@trollbu.de>
2020-11-11 13:46:40 +0000

    Gruppe 3

--- src/main/java/de/cas/mse/exercise/diamond/Diamond.java
+++ src/main/java/de/cas/mse/exercise/diamond/Diamond.java
@@ -3,29 +3,38 @@ package de.cas.mse.exercise.diamond;
 import java.util.Arrays;
 
 public class Diamond {
+    public static void main(String[] args) {
+        System.out.println(new Diamond().generateDiamond(5));
+    }
 
-	public String print(int n) {
-		if (n <= 0 || n % 2 == 0) {
+	public String generateDiamond(int height) {
+		if (!isValidDiamondShape(height)) {
 			return null;
+        }
+        String middleLine = new String(makeLine(height, height));
+        StringBuilder diamond = new StringBuilder(middleLine);
+        // iteratively expand diamond towards top and bottom
+		for (int asterisks = height - 2; asterisks > 0; asterisks -= 2) {
+			char[] line = makeLine(height, asterisks);
+			diamond.insert(0, line);
+			diamond.append(line);
 		}
-		StringBuilder builder = new StringBuilder(new String(make(n, n)));
-		for (int i = n - 2; i > 0; i -= 2) {
-			char[] chars = make(n, i);
-			builder.insert(0, chars);
-			builder.append(chars);
-		}
-		return builder.toString();
-	}
+		return diamond.toString();
+    }
+    
+    private boolean isValidDiamondShape(int height) {
+        return height <= 0 || height % 2 == 0;
+    }
 
-	private char[] make(int i, int j) {
-		int amount = ((i - j) / 2);
-		char[] chars = new char[amount + j + 1];
-		if (amount > 0) {
-			Arrays.fill(chars, 0, amount, ' ');
+	private char[] makeLine(int diamondWidth, int numberOfAsterisks) {
+		int padding = ((diamondWidth - numberOfAsterisks) / 2);
+		char[] line = new char[padding + numberOfAsterisks + 1];
+		if (padding > 0) {
+			Arrays.fill(line, 0, padding, ' ');
 		}
-		Arrays.fill(chars, amount, amount + j, '*');
-		chars[chars.length - 1] = '\n';
-		return chars;
+		Arrays.fill(line, padding, padding + numberOfAsterisks, '*');
+		line[line.length - 1] = '\n';
+		return line;
 	}
 
 }
